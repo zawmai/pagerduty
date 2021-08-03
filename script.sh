2cd47a7a9558b74fab1c2e419c1d2267dfd69976
Zaw Mai <10764631+zawmai@users.noreply.github.com>
2021-08-02 19:59:57 -0700

    Added conditional logic to print error messge

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@
 # Subject: PagerDuty Interview Technical Assignment
 # Last Updated: 08/02/2021
 
-# initialize result string
+# initialize result string and counter
 result=''
+count=0
 
 # skip header lien and start reading from 2nd line.
 while read line;
@@ -16,12 +17,19 @@ do
 
   # if customer equals argument position 2 then append to result string
   # with comma and whitespace delimiter
-  [[ "$customerId" == "$2" ]] && result="$result${result:+, }$pageId"
+  [[ "$customerId" == "$2" ]] && result="$result${result:+, }$pageId" && count=$((count+1))
   
 done < <(tail -n +2 $1)
 
-# print result in a single line
-echo $result
+
+if [[ $((count)) > 0 ]]; then
+  # if there's more than one entry for target customer id
+  # print result in a single line
+  echo $result
+else
+  # else print error message
+  echo "Error: No entries for Customer with id $2"
+fi
 
 # print result line by line: includews duplicates
 # echo $result | grep -o '[^ ]*'
